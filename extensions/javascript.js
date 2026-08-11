1c74080e75d50eda346a60eb7268368d0a968792
katboizz0-arch <katboizz0-arch@gmail.com>
2026-08-11 18:43:42 +0700

    idk

--- extensions/javascript.js
+++ extensions/javascript.js
@@ -126,7 +126,7 @@
       return {
         id: 'jscodeextension',
         name: 'JS Code',
-        color1: '#e5ff00',
+        color1: '#e7e300',
         color2: '#a9ac00',
         blocks: [
           {
