07b76494022e7307eee5da63ca3dd9bf6b24eac5
katboizz0-arch <katboizz0-arch@gmail.com>
2026-09-20 10:00:25 +0700

    fix ext

--- extensions/DANV/WOW-extension.js
+++ extensions/DANV/WOW-extension.js
@@ -98,15 +98,12 @@
     });
   }
 
-  function msg(id, vi, en) {
-    if (typeof Scratch.translate === "function") {
-      const res = Scratch.translate({ id: id, default: en });
-      if (res === en) {
-        const lang = (Scratch.translate.language || navigator.language || "").toLowerCase();
-        if (lang.startsWith("vi")) return vi;
-      }
-      return res;
-    }
+  function msg(id, defaultEnglish) {
+    return Scratch.translate({
+      id: id,
+      default: defaultEnglish
+    });
+  }
     const lang = (navigator.language || "").toLowerCase();
     return lang.startsWith("vi") ? vi : en;
   }
